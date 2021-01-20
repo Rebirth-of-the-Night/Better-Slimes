b1aa4b158c389a240cd29b9f7baea3841111f43e
Colin Wong <cwongmath@gmail.com>
2021-01-19 21:32:53 -0600

    Version bump

--- src/main/java/MICDeps/Reference.java
+++ src/main/java/MICDeps/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MODID = "betterslimes";
     public static final String MODNAME = "Better Slimes";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.3.1";
 
     public static final String CLIENT = "MICDeps.proxy.ClientProxy";
     public static final String SERVER = "MICDeps.proxy.CommonProxy";
